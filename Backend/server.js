3d86d86f31e2bd4f5daeaeaa0b9fddb6e5d4987f
Aman0246 <amankashyap0246@gmail.com>
2023-06-16 17:58:31 +0530

    basic

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+const express=require("express")
+require('dotenv').config()
+var cors = require('cors')
+const app=express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGOCONNECT).then(()=>{console.log("DB connected")}).catch(()=>{console.log("Db not  connected")})
+app.use(express.json())
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running at ${process.env.PORT}`)
+})
